06e921f563a45b105631c01e41eaf998b25be167
LeSamourai23 <ayaan.ahmad@learner.manipal.edu>
2025-02-04 02:28:55 +0530

    Initial

---
+++ script.js
@@ -0,0 +1,27 @@
+const messages = [
+    "Are you sure?",
+    "Really sure?",
+    "Are you positive?",
+    "please... 🥹🥹🥹",
+    "Just think about it!",
+    "If you say no, I will be really sad... 🥹🥹",
+    "I will be very sad.. 🥹🥹🥹🥹",
+    "I will be very very very sad...🥹🥹🥹🥹🥹🥹🥹🥹",
+    "Ok fine, I will stop asking... 😔😔😔",
+    "Just kidding, say yes please! 🥰"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
